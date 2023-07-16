be5c3108a8db0f57af43a62963e3345c46739d9d
Simon <44496148+siim0n@users.noreply.github.com>
2023-07-16 17:36:28 +0200

    add ratelimit

--- src/api.ts
+++ src/api.ts
@@ -3,8 +3,19 @@ import bodyparser from 'body-parser';
 import axios from 'axios';
 import { KeysModel } from './db/models/keys-schema.js';
 import { connect, newApiKeyEvent } from './db/connect.js';
+import { rateLimit } from 'express-rate-limit';
 const app = express()
 app.use(bodyparser.json());
+
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	max: 15, // Limit each IP to 15 requests per `window` (here, per 15 minutes)
+	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
+
+app.use(limiter);
+
 
 app.get('/', function (req, res) {
     res.status(403).send({
