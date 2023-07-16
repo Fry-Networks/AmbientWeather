fdf1019064e7e54e901bfac479125b23439b072c
Simon <44496148+siim0n@users.noreply.github.com>
2023-07-16 16:22:55 +0200

    add 500 if everything errors

--- src/api.ts
+++ src/api.ts
@@ -13,55 +13,63 @@ app.get('/', function (req, res) {
 })
 
 app.post('/api/submitkey', async function (req, res) {
-    const data: {
-        key: string,
-        address: string
-    } = req.body;
-    console.log(data);
-    // Check if the key is already in the database
-    const existingKey = await KeysModel.exists({ api_key: data.key });
+    try {
+        const data: {
+            key: string,
+            address: string
+        } = req.body;
+        console.log(data);
+        // Check if the key is already in the database
+        const existingKey = await KeysModel.exists({ api_key: data.key });
 
-    if (existingKey) {
-        return void res.status(409).send({
-            message: 'Key already exists in database.',
+        if (existingKey) {
+            return void res.status(409).send({
+                message: 'Key already exists in database.',
+                status: 'ERROR'
+            });
+        }
+
+        // Check regex
+        const regexCheck = /^[a-z0-9]{64}$/.test(data.key);
+        if (!regexCheck) {
+            return void res.status(400).send({
+                message: 'Key is invalid. (Didn\'t pass regex check)',
+                status: 'ERROR'
+            });
+        }
+
+        // Check if the key is valid by making a request to the API
+        //https://rt.ambientweather.net/v1/devices?applicationKey=&apiKey=
+        const response = await axios.get(`https://rt.ambientweather.net/v1/devices?applicationKey=${process.env.AW_APPLICATION_KEY}&apiKey=${data.key}`);
+        if (response.status !== 200) {
+            return void res.status(400).send({
+                message: 'Key is invalid. (Didn\'t pass API check)',
+                status: 'ERROR'
+            });
+        }
+
+        // Add the key to the database
+        const key = new KeysModel({
+            api_key: data.key,
+            address: data.address,
+            timestamp: new Date()
+        });
+        await key.save();
+
+        newApiKeyEvent.emit('newApiKey', key._id);
+
+        res.status(200).send({
+            message: 'Successfully linked your API Key to your wallet address!\nWe will soon begin to retreive data from your weather stations/devices.',
+            status: 'SUCCESS'
+        });
+    } catch (e) {
+        res.status(500).send({
+            message: 'Internal server error.',
             status: 'ERROR'
         });
-    }
+    };
 
-    // Check regex
-    const regexCheck = /^[a-z0-9]{64}$/.test(data.key);
-    if (!regexCheck) {
-        return void res.status(400).send({
-            message: 'Key is invalid. (Didn\'t pass regex check)',
-            status: 'ERROR'
-        });
-    }
-
-    // Check if the key is valid by making a request to the API
-    //https://rt.ambientweather.net/v1/devices?applicationKey=&apiKey=
-    const response = await axios.get(`https://rt.ambientweather.net/v1/devices?applicationKey=${process.env.AW_APPLICATION_KEY}&apiKey=${data.key}`);
-    if (response.status !== 200) {
-        return void res.status(400).send({
-            message: 'Key is invalid. (Didn\'t pass API check)',
-            status: 'ERROR'
-        });
-    }
-
-    // Add the key to the database
-    const key = new KeysModel({
-        api_key: data.key,
-        address: data.address,
-        timestamp: new Date()
     });
-    await key.save();
-
-    newApiKeyEvent.emit('newApiKey', key._id);
-
-    res.status(200).send({
-        message: 'Successfully linked your API Key to your wallet address!\nWe will soon begin to retreive data from your weather stations/devices.',
-        status: 'SUCCESS'
-    });
-});
 
 export async function startApi() {
     await connect();
@@ -69,5 +77,5 @@ export async function startApi() {
     app.listen(port, () => {
         console.log(`Listening on port ${port}`);
     });
-    
+
 }
